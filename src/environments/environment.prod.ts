ea9644bee21d653e4af24125f12f276d50b5d0f6
Steven Schaner <schanerst@gmail.com>
2020-03-26 13:56:24 -0400

    add entire environments directory to gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCF5XmgO6bKml5VhPABK6-0ZqKhwGTVtI8",
+    authDomain: "angular-image-gallery-2185d.firebaseapp.com",
+    databaseURL: "https://angular-image-gallery-2185d.firebaseio.com",
+    projectId: "angular-image-gallery-2185d",
+    storageBucket: "angular-image-gallery-2185d.appspot.com",
+    messagingSenderId: "36657313928",
+    appId: "1:36657313928:web:6dea9dcf09b8688270073d",
+    measurementId: "G-1B120YZYD8"
+  }
 };
